0834b114f9504e87a4a90e77b753c5e4ea16cc99
Anna Zuckerman <anna_zuckerman@alumni.brown.edu>
2022-11-13 20:20:56 +0000

    update paths in run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
+# NOTE: directories are referenced here with the expectation that this script
+# will be run from on directory up, using tests/run_tests.sh
+
 set -e  # stop on error
 set -u  # raise error if variable is unset
 set -o pipefail  # fail if any prior step failed
 
 # run pycodestyle on new python scripts used in unit and functional testing
-pycodestyle ../funclib.py
-pycodestyle ../segment.py
-pycodestyle ../test_funclib.py
+pycodestyle funclib.py
+pycodestyle segment.py
+pycodestyle test_funclib.py
 
 # run unit testing
 python -m unittest unit_tests.sh
