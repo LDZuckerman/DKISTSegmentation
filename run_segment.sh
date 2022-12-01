fd30344834e36ccada4702c4c7f33f5cedd115c9
Anna Zuckerman <anna_zuckerman@alumni.brown.edu>
2022-12-01 19:19:40 +0000

    update shell script for new arguements

--- run_segment.sh
+++ run_segment.sh
@@ -7,5 +7,5 @@ set -o pipefail  # fail if any prior step failed
 python segment.py --input_file 'data/IBIS.granulation.aligned.25Apr2019.seq56.sav' --skimage_method 'li' --plot_intermed True --out_file 'output.fits' --vel_comparison_file 'velocity_comparison' --out_dir 'output_IBIS/'
 
 # to run on DKIST data
-#python segment.py --input_file 'data/dkist.cont789nm.scaled.fits' --skimage_method 'li' --plot_intermed True --out_file 'output.fits' --out_dir 'output_DKIST/'
+python segment.py --input_file 'data/dkist.cont789nm.scaled.fits' --skimage_method 'li' --plot_intermed True --out_file 'output.fits' --out_dir 'output_DKIST/'
 
