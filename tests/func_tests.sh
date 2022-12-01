6d8d5767c1709147ef2485c9f29aa4a681c7d5f2
Anna Zuckerman <anna_zuckerman@alumni.brown.edu>
2022-12-01 19:18:57 +0000

    update functional testing for new functionality

--- tests/func_tests.sh
+++ tests/func_tests.sh
@@ -1,23 +1,50 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-
-# check that output plot directory is correct
-run test_segment python segment.py  \
+   
+# --------------- Checks for running on IBIS data ------------------ #
+# check run on IBIS data, with velocity comparison 
+run test_segment_ibis python segment.py \
     --input_file 'data/IBIS.granulation.aligned.25Apr2019.seq56.sav' \
     --skimage_method 'li' \
     --plot_intermed True \
     --out_file 'output.fits' \
-    --out_dir 'output/'
+    --vel_comparison_file 'velocity_comparison' \
+    --out_dir 'output_IBIS_test/'
 
 curr_dir=$(pwd)
-file='/output/intermediate_outputs.png'
-path=$curr_dir$file
+test_dir='/output_IBIS_test'
+file='/intermediate_outputs.png'
+path=$curr_dir$test_dir$file
 # check that the output file actually exists
 if [ -f "$path" ]; then
-   echo ' TEST SUCCEEDED: output plot found in '$path 
+   echo ' TEST SUCCEEDED: intermediate output plot found in '$path 
 else
-   echo ' TEST FAILED: output plot not found in '$path 
+   echo ' TEST FAILED: intermediate output plot not found in '$path 
 fi
 rm $path
 assert_exit_code 0
+
+file='/output.fits'
+path=$curr_dir$test_dir$file
+# check that the output file actually exists
+if [ -f "$path" ]; then
+   echo ' TEST SUCCEEDED: output datafile found in '$path 
+else
+   echo ' TEST FAILED: output datafile not found in '$path 
+fi
+rm $path
+assert_exit_code 0
+
+file='/velocity_comparison.png'
+path=$curr_dir$test_dir$file
+# check that the output file actually exists
+if [ -f "$path" ]; then
+   echo ' TEST SUCCEEDED: velocity comparison plot found in '$path 
+else
+   echo ' TEST FAILED: velocity comparison plot not found in '$path 
+fi
+rm $path
+assert_exit_code 0
+
+rm -r $curr_dir$test_dir
