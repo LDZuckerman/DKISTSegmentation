2159b93b93a509b68333cb0f550a15a3804b29d2
Anna Zuckerman <anna_zuckerman@alumni.brown.edu>
2022-12-01 20:17:50 +0000

    add functional tests for DKIST data case

--- tests/func_tests.sh
+++ tests/func_tests.sh
@@ -48,3 +48,39 @@ rm $path
 assert_exit_code 0
 
 rm -r $curr_dir$test_dir
+
+
+# --------------- Checks for running on DKIST data ------------------ #
+# check run on IBIS data, with velocity comparison 
+run test_segment_ibis python segment.py \
+    --input_file 'data/dkist.cont789nm.scaled.fits' \
+    --skimage_method 'li' \
+    --plot_intermed True \
+    --out_file 'output.fits' \
+    --out_dir 'output_DKIST/'
+
+curr_dir=$(pwd)
+test_dir='/output_DKIST_test'
+file='/intermediate_outputs.png'
+path=$curr_dir$test_dir$file
+# check that the output file actually exists
+if [ -f "$path" ]; then
+   echo ' TEST SUCCEEDED: intermediate output plot found in '$path 
+else
+   echo ' TEST FAILED: intermediate output plot not found in '$path 
+fi
+rm $path
+assert_exit_code 0
+
+file='/output.fits'
+path=$curr_dir$test_dir$file
+# check that the output file actually exists
+if [ -f "$path" ]; then
+   echo ' TEST SUCCEEDED: output datafile found in '$path 
+else
+   echo ' TEST FAILED: output datafile not found in '$path 
+fi
+rm $path
+assert_exit_code 0
+
+rm -r $curr_dir$test_dir
