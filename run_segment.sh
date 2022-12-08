92de7b53216c8762153abe5f8d468a2b566b2838
Anna Zuckerman <anna_zuckerman@alumni.brown.edu>
2022-12-08 05:27:31 +0000

    create variables for resolutions

--- run_segment.sh
+++ run_segment.sh
@@ -4,8 +4,9 @@ set -u  # raise error if variable is unset
 set -o pipefail  # fail if any prior step failed
 
 # to run on IBIS data
-python segment.py --data_path 'data/IBIS' --resolution 0.096 --skimage_method 'li' --plot_intermed True --out_file 'segmented_data' --vel_comparison_file 'velocity_comparison' --out_dir 'example_outputs/IBIS/'
+ibis_res=0.096
+python segment.py --data_path 'data/IBIS' --resolution $ibis_res --skimage_method 'li' --plot_intermed True --out_file 'segmented_data' --vel_comparison_file 'velocity_comparison' --out_dir 'example_outputs/IBIS/'
 
 # to run on DKIST data
-python segment.py --data_path 'data/DKIST' --resolution 0.016 --skimage_method 'li' --plot_intermed True --out_file 'segmented_data' --out_dir 'example_outputs/DKIST/'
-
+dkist_res=0.016
+python segment.py --data_path 'data/DKIST' --resolution $dkist_res --skimage_method 'li' --plot_intermed True --out_file 'segmented_data' --out_dir 'example_outputs/DKIST/'
